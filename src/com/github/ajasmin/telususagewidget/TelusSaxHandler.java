50b0799be0649edac69376e19cbff2b6c3913d81
Alexandre Jasmin <alexandre.jasmin@gmail.com>
2011-04-23 12:39:08 -0400

    Simplify SAX Handler. Remove CSV stuff

--- src/com/github/ajasmin/telususagewidget/TelusSaxHandler.java
+++ src/com/github/ajasmin/telususagewidget/TelusSaxHandler.java
@@ -22,10 +22,8 @@
 
 package com.github.ajasmin.telususagewidget;
 
-import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -33,75 +31,54 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.ext.DefaultHandler2;
 
-import com.github.ajasmin.telususagewidget.repackaged.opencsv.CSVWriter;
-
 public class TelusSaxHandler extends DefaultHandler2 {
 	private Map<String, Map<String, String>> data;
 	private List<String> columns;
 	private String currentHeading;
-	private LinkedList<String> tableClasses;
 	
 	private boolean isLoginError;
 	
     private StringBuilder builder;
-    
-    private StringWriter writer;
-    private CSVWriter csv;
 
 	@Override
 	public void startDocument() throws SAXException {
 		data = new HashMap<String, Map<String,String>>();
 		columns = new ArrayList<String>();
 		currentHeading = "";
-		tableClasses = new LinkedList<String>();
 		
 		isLoginError = false;
 		
         builder = new StringBuilder();
-
-        writer = new StringWriter();
-        csv = new CSVWriter(writer);
 	}
 	
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
 		builder.setLength(0);
-		if (localName.equals("table")) {
-			tableClasses.addLast(attributes.getValue("class"));
-		}
-	};
+	}
 	
 	@Override
 	public void endElement(String uri, String localName, String qName) throws SAXException {
 		String trimedText = builder.toString().trim();
 		if (localName.equals("b")) {
 			currentHeading = trimedText;
-			
-			csv.writeNext(new String[] {currentHeading});
-		} else if (localName.equals("td")) {
-			columns.add(trimedText);
-		} else if (localName.equals("tr")) {
-			if (!"quickNavList".equals(tableClasses.getLast())) {
-				Map<String, String> underHeading = data.get(currentHeading);
-				if (underHeading == null) {
-					underHeading = new HashMap<String, String>();
-					data.put(currentHeading, underHeading);
-				}
-				underHeading.put(columns.get(0), columns.size() > 0 ? columns.get(1) : "");
-			
-				String[] csvRow = new String[columns.size()+1];
-				csvRow[0] = "";
-				System.arraycopy(columns.toArray(), 0, csvRow, 1, columns.size());
-				csv.writeNext(csvRow);
-			}
-			columns.clear();
 		} else if (localName.equals("table")) {
-			tableClasses.removeLast();
 			currentHeading = "";
 		} else if (localName.equals("div")) {
 			if (trimedText.equals("The email or password you entered is invalid.  Please try again.")) {
 				isLoginError = true;
 			}
+		} else if (localName.equals("td")) {
+			columns.add(trimedText);
+		} else if (localName.equals("tr")) {
+			if (columns.size() > 0) {
+				Map<String, String> underHeading = data.get(currentHeading);
+				if (underHeading == null) {
+					underHeading = new HashMap<String, String>();
+					data.put(currentHeading, underHeading);
+				}
+				underHeading.put(columns.get(0), columns.size() > 1 ? columns.get(1) : "");
+			}
+			columns.clear();
 		}
 	}
 
@@ -113,10 +90,6 @@ public class TelusSaxHandler extends DefaultHandler2 {
     
     public Map<String, Map<String, String>> getData() {
     	return data;
-    }
-    
-    public String getCsv() {
-    	return writer.toString();
     }
     
     public boolean isLoginError() {
