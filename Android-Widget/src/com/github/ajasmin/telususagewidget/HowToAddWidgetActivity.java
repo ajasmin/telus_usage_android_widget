4cba5e7dc672e947f18008f723f0bfeca8d4ac9a
Alexandre Jasmin <alexandre.jasmin@gmail.com>
2011-05-05 17:14:16 -0400

    Show step by step instructions when installing from the Market

---
+++ Android-Widget/src/com/github/ajasmin/telususagewidget/HowToAddWidgetActivity.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2011 by Alexandre Jasmin <alexandre.jasmin@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.github.ajasmin.telususagewidget;
+
+import java.io.InputStream;
+
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Movie;
+import android.graphics.Paint;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.Html;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+public class HowToAddWidgetActivity extends Activity {
+	private Movie movie;
+	private long movieStart;
+	private ImageView animated_image;
+	protected Handler animate_handler;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.how_to_add_widget);
+		
+		TextView howToAddText = (TextView) findViewById(R.id.how_to_add_text);
+        howToAddText.setText(Html.fromHtml(getString(R.string.how_to_add)));
+        
+        animated_image = (ImageView) findViewById(R.id.animated_image);
+
+        if (movie == null) {
+	        InputStream is = getResources().openRawResource(R.drawable.how_to_add_anim_gif);
+	        movie = Movie.decodeStream(is);
+        }
+        if (savedInstanceState != null)
+        	movieStart = savedInstanceState.getLong("movieStart");
+        animate_handler = new Handler();
+        animate_handler.postDelayed(animate, 30);
+    }
+	
+	protected void onSaveInstanceState(Bundle outState) {
+		outState.putLong("movieStart", movieStart);
+	};
+	
+	Runnable animate = new Runnable() { public void run() {
+        Bitmap offscreenBmp = Bitmap.createBitmap(240, 320, Bitmap.Config.ARGB_8888);
+        long now = android.os.SystemClock.uptimeMillis();
+        if (movieStart == 0) {   // first time
+            movieStart = now;
+        }
+        if (movie != null) {
+            int dur = movie.duration();
+            if (dur == 0) {
+                dur = 1000;
+            }
+            int relTime = (int)((now - movieStart) % dur);
+            movie.setTime(relTime);
+            movie.draw(new Canvas(offscreenBmp), 0, 0);
+            animated_image.setImageBitmap(offscreenBmp);
+        }
+        animate_handler = new Handler();
+        animate_handler.postDelayed(animate, 30);
+	}};
+}
