c0e3647e662e6247c677ef10bc24c691f5833fd4
Alexandre Jasmin <alexandre.jasmin@gmail.com>
2011-05-06 05:32:15 -0400

    Replace animated_gif by PNG sequence because of Issue 2766 on older devices

--- Android-Widget/src/com/github/ajasmin/telususagewidget/HowToAddWidgetActivity.java
+++ Android-Widget/src/com/github/ajasmin/telususagewidget/HowToAddWidgetActivity.java
@@ -27,10 +27,12 @@ import java.io.InputStream;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Movie;
 import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnBufferingUpdateListener;
 import android.net.Uri;
@@ -46,11 +48,10 @@ import android.widget.TextView;
 import android.widget.VideoView;
 
 public class HowToAddWidgetActivity extends Activity {
-	private Movie movie;
-	private long movieStart;
+	int framePos = 0;
 	private ImageView animated_image;
 	protected Handler animate_handler;
-	Bitmap animationFrame = Bitmap.createBitmap(240, 320, Bitmap.Config.ARGB_8888);
+	Bitmap animationFrame;
 	private boolean isAnimating;
 	
 	@Override
@@ -71,16 +72,15 @@ public class HowToAddWidgetActivity extends Activity {
         
         animated_image = (ImageView) findViewById(R.id.animated_image);
 
-        if (movie == null) {
-	        InputStream is = getResources().openRawResource(R.drawable.how_to_add_anim_gif);
-	        movie = Movie.decodeStream(is);
+        if (savedInstanceState != null) {
+        	animationFrame = savedInstanceState.getParcelable("animationFrame");
+        	framePos = savedInstanceState.getInt("framePos");
         }
-        if (savedInstanceState != null)
-        	movieStart = savedInstanceState.getLong("movieStart");
     }
 	
 	protected void onSaveInstanceState(Bundle outState) {
-		outState.putLong("movieStart", movieStart);
+		outState.putInt("framePos", framePos);
+		outState.putParcelable("animationFrame", animationFrame);
 	};
 	
 	protected void onResume() {
@@ -88,7 +88,7 @@ public class HowToAddWidgetActivity extends Activity {
 		
 		isAnimating = true;
         animate_handler = new Handler();
-        animate_handler.postDelayed(animate, 30);
+        animate_handler.postDelayed(animate, 66);
 	}
 
 	protected void onPause() {
@@ -100,21 +100,24 @@ public class HowToAddWidgetActivity extends Activity {
 	Runnable animate = new Runnable() { public void run() {
 		if (!isAnimating)
 			return;
-        long now = android.os.SystemClock.uptimeMillis();
-        if (movieStart == 0) {   // first time
-            movieStart = now;
-        }
-        if (movie != null) {
-            int dur = movie.duration();
-            if (dur == 0) {
-                dur = 1000;
-            }
-            int relTime = (int)((now - movieStart) % dur);
-            movie.setTime(relTime);
-            movie.draw(new Canvas(animationFrame), 0, 0);
-            animated_image.setImageBitmap(animationFrame);
-        }
+
+		if (animated_image.getWidth() > 0) {
+			BitmapDrawable drawable = (BitmapDrawable) getResources().getDrawable(frames[framePos]);
+			Bitmap bitmap = drawable.getBitmap();
+
+			if (animationFrame == null) {
+				animationFrame = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Config.ARGB_8888);
+			}
+			
+			Canvas canvas = new Canvas(animationFrame);
+			canvas.drawBitmap(bitmap, 0, 0, null);
+			
+			animated_image.setImageBitmap(animationFrame);
+			framePos = ++framePos % frames.length;
+		}
+        
         animate_handler = new Handler();
-        animate_handler.postDelayed(animate, 30);
+        animate_handler.postDelayed(animate, 66);
 	}};
+	private static final int[] frames = {R.drawable.anim_how00000001, R.drawable.anim_how00000002, R.drawable.anim_how00000002, R.drawable.anim_how00000002, R.drawable.anim_how00000002, R.drawable.anim_how00000002, R.drawable.anim_how00000002, R.drawable.anim_how00000002, R.drawable.anim_how00000009, R.drawable.anim_how00000009, R.drawable.anim_how00000011, R.drawable.anim_how00000011, R.drawable.anim_how00000011, R.drawable.anim_how00000011, R.drawable.anim_how00000015, R.drawable.anim_how00000016, R.drawable.anim_how00000017, R.drawable.anim_how00000018, R.drawable.anim_how00000019, R.drawable.anim_how00000020, R.drawable.anim_how00000021, R.drawable.anim_how00000022, R.drawable.anim_how00000022, R.drawable.anim_how00000024, R.drawable.anim_how00000025, R.drawable.anim_how00000025, R.drawable.anim_how00000027, R.drawable.anim_how00000028, R.drawable.anim_how00000029, R.drawable.anim_how00000030, R.drawable.anim_how00000030, R.drawable.anim_how00000030, R.drawable.anim_how00000033, R.drawable.anim_how00000034, R.drawable.anim_how00000035, R.drawable.anim_how00000036, R.drawable.anim_how00000037, R.drawable.anim_how00000038, R.drawable.anim_how00000039, R.drawable.anim_how00000040, R.drawable.anim_how00000040, R.drawable.anim_how00000040, R.drawable.anim_how00000040, R.drawable.anim_how00000040, R.drawable.anim_how00000040, R.drawable.anim_how00000040, R.drawable.anim_how00000047, R.drawable.anim_how00000047, R.drawable.anim_how00000049, R.drawable.anim_how00000049, R.drawable.anim_how00000051, R.drawable.anim_how00000051, R.drawable.anim_how00000053, R.drawable.anim_how00000054, R.drawable.anim_how00000055, R.drawable.anim_how00000056, R.drawable.anim_how00000056, R.drawable.anim_how00000058, R.drawable.anim_how00000058, R.drawable.anim_how00000058, R.drawable.anim_how00000058, R.drawable.anim_how00000062, R.drawable.anim_how00000062, R.drawable.anim_how00000062, R.drawable.anim_how00000062, R.drawable.anim_how00000066, R.drawable.anim_how00000066, R.drawable.anim_how00000068, R.drawable.anim_how00000069, R.drawable.anim_how00000070, R.drawable.anim_how00000070, R.drawable.anim_how00000070, R.drawable.anim_how00000070, R.drawable.anim_how00000074, R.drawable.anim_how00000074, R.drawable.anim_how00000074, R.drawable.anim_how00000074, R.drawable.anim_how00000078, R.drawable.anim_how00000079, R.drawable.anim_how00000080, R.drawable.anim_how00000080, R.drawable.anim_how00000080, R.drawable.anim_how00000083, R.drawable.anim_how00000084, R.drawable.anim_how00000085, R.drawable.anim_how00000085, R.drawable.anim_how00000087, R.drawable.anim_how00000088, R.drawable.anim_how00000088, R.drawable.anim_how00000090, R.drawable.anim_how00000091, R.drawable.anim_how00000091, R.drawable.anim_how00000091, R.drawable.anim_how00000094, R.drawable.anim_how00000094, R.drawable.anim_how00000094, R.drawable.anim_how00000094, R.drawable.anim_how00000098, R.drawable.anim_how00000098, R.drawable.anim_how00000100, R.drawable.anim_how00000101, R.drawable.anim_how00000102, R.drawable.anim_how00000103, R.drawable.anim_how00000104, R.drawable.anim_how00000105, R.drawable.anim_how00000105, R.drawable.anim_how00000105, R.drawable.anim_how00000108, R.drawable.anim_how00000109, R.drawable.anim_how00000109, R.drawable.anim_how00000109, R.drawable.anim_how00000112, R.drawable.anim_how00000112, R.drawable.anim_how00000112, R.drawable.anim_how00000115, R.drawable.anim_how00000116, R.drawable.anim_how00000117, R.drawable.anim_how00000117, R.drawable.anim_how00000117, R.drawable.anim_how00000120, R.drawable.anim_how00000121, R.drawable.anim_how00000121, R.drawable.anim_how00000123, R.drawable.anim_how00000123, R.drawable.anim_how00000123, R.drawable.anim_how00000123, R.drawable.anim_how00000127, R.drawable.anim_how00000127, R.drawable.anim_how00000127, R.drawable.anim_how00000127, R.drawable.anim_how00000131, R.drawable.anim_how00000131, R.drawable.anim_how00000131, R.drawable.anim_how00000134, R.drawable.anim_how00000134, R.drawable.anim_how00000134, R.drawable.anim_how00000137, R.drawable.anim_how00000138, R.drawable.anim_how00000139, R.drawable.anim_how00000139, R.drawable.anim_how00000141, R.drawable.anim_how00000141, R.drawable.anim_how00000143, R.drawable.anim_how00000144, R.drawable.anim_how00000145, R.drawable.anim_how00000146, R.drawable.anim_how00000147, R.drawable.anim_how00000148, R.drawable.anim_how00000149, R.drawable.anim_how00000150, R.drawable.anim_how00000151, R.drawable.anim_how00000152, R.drawable.anim_how00000152, R.drawable.anim_how00000152, R.drawable.anim_how00000152, R.drawable.anim_how00000152, R.drawable.anim_how00000152, R.drawable.anim_how00000152, R.drawable.anim_how00000152, R.drawable.anim_how00000152, R.drawable.anim_how00000152, R.drawable.anim_how00000162, R.drawable.anim_how00000162, R.drawable.anim_how00000162, R.drawable.anim_how00000162, R.drawable.anim_how00000162, R.drawable.anim_how00000162, R.drawable.anim_how00000162, R.drawable.anim_how00000162, R.drawable.anim_how00000170, R.drawable.anim_how00000170, R.drawable.anim_how00000170, R.drawable.anim_how00000170, R.drawable.anim_how00000170, R.drawable.anim_how00000170, R.drawable.anim_how00000170, R.drawable.anim_how00000177, R.drawable.anim_how00000177, R.drawable.anim_how00000177, R.drawable.anim_how00000177, R.drawable.anim_how00000177, R.drawable.anim_how00000177, R.drawable.anim_how00000177, R.drawable.anim_how00000177, R.drawable.anim_how00000177, R.drawable.anim_how00000177, R.drawable.anim_how00000177, R.drawable.anim_how00000177, R.drawable.anim_how00000177, R.drawable.anim_how00000177, R.drawable.anim_how00000177, R.drawable.anim_how00000177};
 }
